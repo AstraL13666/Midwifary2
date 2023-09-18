bd20a4d49f629b279f58ee0984916c2e3cfdede4
V. Kotov <vlad13kotov@gmail.com>
2023-09-18 22:03:04 +0300

    refactor

--- handler/chat/standart_command.py
+++ handler/chat/standart_command.py
@@ -1,12 +1,14 @@
+from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters.command import CommandStart
 
 
+router = Router(name="user")
+
+
+@router.message(CommandStart())
 async def command_start(m: Message):
     await m.answer(
         text='Hi'
     )
 
-
-def register_message_handlers(dp):
-    dp.message.register(command_start, CommandStart())
